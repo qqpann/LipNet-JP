1f03cfa770cb0261ecf230118d106e4f194ffd1b
itumizu <s1611488@klis.tsukuba.ac.jp>
2019-10-26 18:50:45 +0900

    Fix: socket.io server

--- server/server.py
+++ server/server.py
@@ -1,7 +1,6 @@
 import eventlet
 import socketio
 import numpy as np
-import logging
 import cv2
 
 sio = socketio.Server()
@@ -12,35 +11,41 @@ app = socketio.WSGIApp(sio, static_files={
 buffer = {}
 np.set_printoptions(threshold=10000000)
 
+# 予測モデルで予測を行うメソッド
+def requestPrediction(bufferImage):
+    return ["まいたけ"]
+
 @sio.event
 def connect(sid, environ):
-    logging.debug('connect ', sid)
+    print('connect ', sid)
     buffer[sid] = []
 
 @sio.event
-def my_message(sid, data):
+def disconnect(sid):
+    print('disconnect ', sid)
+
+@sio.event
+def message(sid, data):
     print('message ', data)
 
 @sio.event
 def sendImage(sid, data):
     data = np.frombuffer(data, dtype=np.uint8)
-    # print(data)
     data = np.reshape(data, (160,80,3))
-
-    # k = cv2.waitKey(1)
     buffer[sid].append(data)
 
 # 画像配列を変換候補メソッドへ渡す
-def predictMouth(sid, data):
-    bufferImage = np.array(data[sid])
-    # 変換候補メソッドへ投げる
-    
-    # 返ってきた値を返す
-    sio.emit('requestPredictMouth', {'data': [""]}, room=sid)
-
 @sio.event
-def disconnect(sid):
-    logging.debug('disconnect ', sid)
+def predictMouth(sid, data):
+    bufferImage = np.array(buffer[sid])
+    
+    # 予測メソッドへ投げる
+    response = requestPrediction(bufferImage)
+
+    # 返ってきた値を返す
+    sio.emit('requestPredictMouth', {'data': response}, room=sid)
+    
+    buffer[sid] = []
 
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 80)), app)
